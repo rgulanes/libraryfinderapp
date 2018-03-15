70ef9ebe712d3ead8685231b97d7495ab63031c0
Ronald Jay Gulanes <rjlgulanes@gcg.gov.ph>
2018-03-15 15:23:12 +0800

    Add prompt that app is offline;

--- platforms/browser/www/js/libraryfinderapp-globals.js
+++ platforms/browser/www/js/libraryfinderapp-globals.js
@@ -3,21 +3,38 @@ $(document).bind("mobileinit", function(){
   $.mobile.allowCrossDomainPages = true;
 });
 
-
 // Wait for device API libraries to load
 //
 function onLoad() {
-    document.addEventListener("deviceready", onDeviceReady, false);
+  document.addEventListener("deviceready", onDeviceReady, false);
 }
+
 // device APIs are available
 //
 function onDeviceReady() {
-    // Register the event listener
-    document.addEventListener("backbutton", onBackKeyDown, false);
+  // Register the event listener
+  document.addEventListener("backbutton", onBackKeyDown, false);
+  document.addEventListener("offline", isOffline, false);
 }
 // Handle the back button
 //
 function onBackKeyDown() {
-    alert('Backbutton key pressed');
-    console.log('Backbutton key pressed');
+  console.log('Backbutton key pressed');
+}
+// function if application is offline
+function isOffline() {
+  swal({
+    title: 'Application is Offline',
+    html: "You are about to use the application in an offline status.',
+    type: 'warning',
+    showCancelButton: false,
+    confirmButtonColor: '#3085d6',
+    allowOutsideClick: false,
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Confirm'
+  }).then((result) => {
+    if (result.value) {
+      swal.close();
+    }
+  });
 }
