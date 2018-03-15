e8cdf32d1f181600528d2f727c1eb21aa20229e1
Ronald Jay Gulanes <rjlgulanes@gcg.gov.ph>
2018-03-15 15:05:55 +0800

    Added onload event handler;

--- platforms/browser/www/js/libraryfinderapp-globals.js
+++ platforms/browser/www/js/libraryfinderapp-globals.js
@@ -9,14 +9,12 @@ $(document).bind("mobileinit", function(){
 function onLoad() {
     document.addEventListener("deviceready", onDeviceReady, false);
 }
-
 // device APIs are available
 //
 function onDeviceReady() {
     // Register the event listener
     document.addEventListener("backbutton", onBackKeyDown, false);
 }
-
 // Handle the back button
 //
 function onBackKeyDown() {
